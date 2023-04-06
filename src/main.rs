34fce2e0f613347e6dfbccb433a4edd2d2bd86c6
arya <arya.abawi@gmail.com>
2023-04-06 10:36:24 +0800

    switching the model to cheetah

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,34 @@
-use coqui_stt::{Model, Stream};
-use std::fs::File;
-use std::io::Read;
+use cheetah::{CheetahBuilder, Cheetah};
+use std::env;
+use dotenv::dotenv;
 
 fn main() {
-    let model_path = "assets/models/deepspeech-0.9.3-models.pbmm";
-    let scorer_path = "assets/scorer/deepspeech-0.9.3-models.scorer";
-    let audio_path = "assets/test-audio/2830-3980-0043.wav";
+    dotenv().ok();
 
-    // Load the CoquiSTT model
-    let mut model = Model::new(model_path).expect("Failed to load the model");
-    model.enable_external_scorer(scorer_path).expect("Failed to enable the scorer");
+    let audio_path = "/Users/arya/projects/speech-to-text-chatgpt/assets/test-audiio/2830-3980-0043.wav";
+
+    let access_key = env::var("PICO_ACCESS_KEY").unwrap();
+
+    // Create a Picovoice instance with the Cheetah ASR model
+    let cheetah: Cheetah = CheetahBuilder::new()
+        .access_key(access_key)
+        .init()
+        .expect("Unable to create Cheetah");
+
 
     // Read the audio file
-    let mut audio_file = File::open(audio_path).expect("Failed to open the audio file");
-    let mut audio_buffer = Vec::new();
-    audio_file
-        .read_to_end(&mut audio_buffer)
-        .expect("Failed to read the audio file");
+    fn next_audio_frame(audio_path: &str) -> Vec<i16> {
+        let mut reader = hound::WavReader::open(audio_path).expect("Failed to open the audio file");
+        let audio_buffer: Vec<i16> = reader.samples::<i16>().map(Result::unwrap).collect();
+        return audio_buffer
+    }
 
-    // Set up the streaming state
-    let mut streaming_state = Stream::from_model(&mut model).expect("Failed to create the streaming state");
-
-    // Feed the audio buffer to the streaming state
-    streaming_state.feed_audio(&audio_buffer);
-
-    // Convert the audio to text
-    let transcript = streaming_state.finish().expect("Failed to convert speech to text");
-
-    println!("Transcript: {}", transcript);
+    if let Ok(cheetah_transcript) = cheetah.process(&next_audio_frame(&audio_path)) {
+        println!("{}", cheetah_transcript.transcript);
+        if cheetah_transcript.is_endpoint {
+          if let Ok(cheetah_transcript) = cheetah.flush() {
+            println!("{}", cheetah_transcript.transcript)
+          }
+        }
+      }
 }
