50bb149c8f33239cdff8826606b21051fe434246
arya <arya.abawi@gmail.com>
2023-04-14 09:57:57 +0800

    abstracted audio setup into lib file

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,23 @@
 pub mod audio_input;
 pub mod recorder;
 pub mod gpt_requests;
-pub mod message_broker;
+pub mod dialog_broker;
+
+use recorder::run_recorder;
+
+use crate::audio_input::{show_audio_devices, get_audio_index_from_user};
+
+pub fn audio_input_setup() -> i32 {
+    let mut idx: i32 = -1;
+    print!("Hey. Please select an input audio device by typing in its index\n");
+    let audio_devices = show_audio_devices();
+    get_audio_index_from_user(&mut idx, &audio_devices);
+
+    println!("You selected {}", audio_devices.get(idx as usize).unwrap());
+    return idx
+}
+
+pub fn recorder_setup() {
+    let idx = audio_input_setup();
+    run_recorder(idx)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use speech_to_text_chatgpt::{
-    audio_input::*, 
-    recorder::*
+    recorder_setup
 };
 
 use dotenv::dotenv;
@@ -8,12 +7,5 @@ use dotenv::dotenv;
 fn main() {
     dotenv().ok();
 
-    let mut idx: i32 = -1;
-    print!("Hey. Please select an input audio device by typing in its index\n");
-    let audio_devices = show_audio_devices();
-    get_audio_index_from_user(&mut idx, &audio_devices);
-
-    println!("You selected {}", audio_devices.get(idx as usize).unwrap());
-
-    run_recorder(idx)
+    recorder_setup()
 }
