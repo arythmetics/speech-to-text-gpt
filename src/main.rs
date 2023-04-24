effb3596ec7eb7e2c74cacfcbe168765982a4fb0
arya <arya.abawi@gmail.com>
2023-04-24 12:09:49 +0800

    experimenting with user input

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 use dotenv::dotenv;
 use std::sync::atomic::{AtomicBool, Ordering};
+use crossterm::{
+    event::{Event, KeyCode, KeyEvent, KeyModifiers},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+};
+use std::io::{stdout, Write};
 
 use speech_to_text_chatgpt::{
     dialog_broker::DialogBroker, 
@@ -21,8 +27,39 @@ fn main() {
 
     let idx = audio_input_setup();
 
-    while RUNNING.load(Ordering::SeqCst) {
-        run_recorder(idx, &RECORDING, &mut dialog_broker);
-        println!("{}", dialog_broker.user_content)
+    let mut stdout = stdout();
+
+    execute!(stdout, EnterAlternateScreen).unwrap();
+    enable_raw_mode().unwrap();
+
+    loop {
+        if let Ok(key_event) = crossterm::event::read() {
+            match key_event {
+                Event::Key(KeyEvent {
+                    code: KeyCode::Char(' '),
+                    modifiers,
+                    ..
+                }) => {
+                    if modifiers == KeyModifiers::empty() {
+                        print!("hello");
+                        stdout.flush().unwrap();
+                    }
+                }
+                Event::Key(KeyEvent {
+                    code: KeyCode::Char('q'),
+                    ..
+                }) => {
+                    break;
+                }
+                _ => {}
+            }
+        }
     }
+
+    disable_raw_mode().unwrap();
+    execute!(stdout, LeaveAlternateScreen).unwrap();
+
+    // while RUNNING.load(Ordering::SeqCst) {
+    //     run_recorder(idx, &RECORDING, &mut dialog_broker);
+    // }
 }
